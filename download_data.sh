7163bc8a8494feb4f5f5fb34180c31b0e8f02313
Gary Burnett <gburnett@nvidia.com>
2025-03-04 06:09:40 -0800

    Uncommenting download data script

--- download_data.sh
+++ download_data.sh
@@ -1,18 +1,18 @@
 
-# mkdir -p data/ref
+mkdir -p data/ref
 
-# # Exome files 
-# cd data && \
-#     wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz && \
-#     wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
+# Exome files 
+cd data && \
+    wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz && \
+    wget ftp://ftp-trace.ncbi.nih.gov/ReferenceSamples/giab/data/NA12878/Garvan_NA12878_HG001_HiSeq_Exome/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz
 
-# # Reference files 
-# cd - 
-# wget -O parabricks_sample.tar.gz https://s3.amazonaws.com/parabricks.sample/parabricks_sample.tar.gz
-# tar xzvf parabricks_sample.tar.gz
+# Reference files 
+cd - 
+wget -O parabricks_sample.tar.gz https://s3.amazonaws.com/parabricks.sample/parabricks_sample.tar.gz
+tar xzvf parabricks_sample.tar.gz
 
-# mv parabricks_sample/Ref parabricks_sample/ref
-# mv parabricks_sample/ref data
+mv parabricks_sample/Ref parabricks_sample/ref
+mv parabricks_sample/ref data
 
 # Remove unnecessary files 
 rm -rf parabricks_sample*

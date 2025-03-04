9b3b94111315dd91f62bc2a1f97f8e8ee179a9d2
Taurean Dyer <tdyer@nvidia.com>
2025-03-03 16:55:17 -0800

    updates to fix kernel crashing and overallflow

---
+++ pb_alignment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+REF="data/ref/Homo_sapiens_assembly38.fasta"
+FASTQ_1="data/NIST7035_TAAGGCGA_L001_R1_001.fastq.gz"
+FASTQ_2="data/NIST7035_TAAGGCGA_L001_R2_001.fastq.gz"
+OUT_BAM="data/NIST7035_TAAGGCGA_L001_R1_001.bam"
+
+pbrun fq2bam \
+    --ref ${REF} \
+    --in-fq ${FASTQ_1} ${FASTQ_2} \
+    --out-bam ${OUT_BAM}
